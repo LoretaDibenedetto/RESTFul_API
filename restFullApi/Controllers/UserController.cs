df814f9026ebc1f60d2b23d3f7d36b9fa3936438
LoretaDibenedetto <dibenedettoloreta@gmail.com>
2023-07-13 10:54:37 +0200

    Aggiungere i file di progetto.

---
+++ restFullApi/Controllers/UserController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace restFullApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        [HttpGet] 
+        public IActionResult GetData()
+        {
+            return Ok("Test");
+        }
+    }
+}
